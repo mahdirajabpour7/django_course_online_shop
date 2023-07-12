f8c335c77b20001ae9e725a917ca98ac4b19a431
mahdi <mahdirajabpour1380@gmail.com>
2023-07-13 01:09:12 +0330

    change some accounts templates

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'DJANGO_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["localhost" , "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
